c1ba03abaab2f173c160d4d4f1eba2474c3a302d
DiogoBarrosx <diogobarrosx@gmail.com>
2021-01-13 23:06:12 -0300

    primeira aula js dio - avanade

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var num1 = 8;
+var num2 = 2;
+
+console.log("adição: " + (num1 + num2));
+console.log("subtração: " + (num1 - num2));
+console.log("multiplicação: " + (num1 * num2));
+console.log("divisão: " + (num1 / num2));
---
+++ js/main2.js
@@ -0,0 +1,32 @@
+var nome = "Diogo Barbosa";
+var idade = 27;
+
+//comentário de linha
+
+/* comentário em bloco */
+
+/** strings **/
+
+
+alert(nome + " tem " + idade + " anos!");
+
+//console.log(nome + " tem " + idade + " anos!");
+
+//console.log(nome.toLowerCase());
+
+//console.log(nome.toUpperCase());
+
+//console.log(nome.replace("Barbosa", "Barros"));
+
+
+/** operações aritméticas **/
+
+var num1 = 8;
+var num2 = 2;
+
+
+
+console.log("adição: " + (num1 + num2));
+console.log("subtração: " + (num1 - num2));
+console.log("multiplicação: " + (num1 * num2));
+console.log("divisão: " + (num1 / num2));

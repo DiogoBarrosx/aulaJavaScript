bd61cc5f58ec28b7fcb1f3e8a3569f95a14ba331
DiogoBarrosx <diogobarrosx@gmail.com>
2021-01-14 22:32:33 -0300

    segunda aula avanade - dio - array e dicionario

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+/** Dicionário */
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas);
+console.log(frutas[0].nome);
+console.log(frutas[0].cor);
+
+/** 
+var fruta = {nome:"maça" , cor:"vermelha"}
+//console.log(fruta.nome);
+//console.log(fruta.cor);
+console.log(fruta);
+*/
+
+
+
+/** ARRAY */
+/** 
+var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+
+/**
+ * 
 var num1 = 8;
 var num2 = 2;
 
@@ -5,3 +36,5 @@ console.log("adição: " + (num1 + num2));
 console.log("subtração: " + (num1 - num2));
 console.log("multiplicação: " + (num1 * num2));
 console.log("divisão: " + (num1 / num2));
+*
+*/
--- js/main2.js
+++ js/main2.js
@@ -1,16 +1,15 @@
-var nome = "Diogo Barbosa";
-var idade = 27;
-
 //comentário de linha
 
 /* comentário em bloco */
 
-/** strings **/
+/** variáveis strings e concatenação **/
 
+var nome = "Diogo Barbosa";
+var idade = 27;
 
 alert(nome + " tem " + idade + " anos!");
 
-//console.log(nome + " tem " + idade + " anos!");
+console.log(nome + " tem " + idade + " anos!");
 
 //console.log(nome.toLowerCase());
 
@@ -19,14 +18,40 @@ alert(nome + " tem " + idade + " anos!");
 //console.log(nome.replace("Barbosa", "Barros"));
 
 
+
 /** operações aritméticas **/
 
 var num1 = 8;
 var num2 = 2;
 
-
-
 console.log("adição: " + (num1 + num2));
 console.log("subtração: " + (num1 - num2));
 console.log("multiplicação: " + (num1 * num2));
 console.log("divisão: " + (num1 / num2));
+
+
+
+/** Array */
+
+var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+
+/** Dicionário */
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+//console.log(frutas[0].nome);
+//console.log(frutas[0].cor);
+
+var fruta = {nome:"maça" , cor:"vermelha"}
+//console.log(fruta.nome);
+//console.log(fruta.cor);
+console.log(fruta);
+
